d22b098a569ed3a29f08911af1a193266be7daee
Mooltissm <43393891+Mooltissm@users.noreply.github.com>
2019-09-30 21:58:14 +0800

    Still fixing boss

--- Source/ParticleTest/Enemy.cpp
+++ Source/ParticleTest/Enemy.cpp
@@ -8,7 +8,7 @@ AEnemy::AEnemy()
 {
  	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
-
+	CanBeTargeted = true;
 }
 
 // Called when the game starts or when spawned
